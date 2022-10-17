55dc675e998b598ee2b9999031e0ac5036174938
akshaywebteasor <akshay.webteasor@gmail.com>
2022-10-17 16:31:22 +0530

    error handling

--- controllers/associatejob.controller.js
+++ controllers/associatejob.controller.js
@@ -3,7 +3,6 @@ const associatejobdatas = require('../models/associatejobdatas')
 
 module.exports = {
     'getassociatedjob_data' : async(req, res) => {
-        
         const { page = 1, limit = 5 } = req.query;
         try {
             console.log('withoutid')
@@ -11,29 +10,64 @@ module.exports = {
             .limit(limit * 1)
             .skip((page - 1) * limit)
             .exec();
-        
+            if(!data){
+                console.log('noid')
+                data={
+                    "status": 404,
+                    "errors": [
+                        'You have no data'
+                    ]
+                }
+            }
         var count = await associatejobdatas.count();
         res.json({
             data,
             totalPages: Math.ceil(count / limit),
             currentPage: page
           });
-        //return res.send(data);
         } catch (err) {
-            console.error(err.message);
+            data={
+                "status": 500,
+                "errors": [
+                    'Something went worng'
+                ]
+            }
+            res.json(data)
         }
     },
     'getassociatedjob_data_by_id' : async(req, res) => {
-        data={
-            "status": 404,
-            "errors": [
-                'Id not found'
-            ]
-        }
         var id = req.params.id;
-        var data = await associatejobdatas.findOne({id : id})
-        console.log("data: ",JSON.parse(JSON.stringify(data)))
+        try {
+        if(!id){
+            console.log('noid')
+            data={
+                "status": 404,
+                "errors": [
+                    'No Id found'
+                ]
+            }
+            return res.json(data)
+        }
+        var data = await associatejobdatas.findOne({_id : id})
+        if(!data){
+            console.log('noid')
+            data={
+                "status": 404,
+                "errors": [
+                    'No data found'
+                ]
+            }
+        }
         res.json(data)
+        } catch (error) {
+            data={
+                "status": 500,
+                "errors": [
+                    'Something went worng'
+                ]
+            }
+            res.json(data)
+        }
 
     }
 }
